8dfa7891ac687795c158fe7184998f76ac9f2aa5
LiangJinJun <7763693+liangjinjun@user.noreply.gitee.com>
2020-10-08 02:17:33 +0800

    完成新闻模块

---
+++ pages/comment/comment.js
@@ -0,0 +1,53 @@
+Page({
+  data: {
+    list: [{
+        id: 1,
+        image: '/assets/image/ljj.jpg',
+        name: '陈奕迅',
+        value: '大伊山、伊甸园、潮河湾、伊芦山……这个季节的灌云，也太美了叭！灌云县地处江苏省东北部、新沂河北岸',
+        calendar: "2020.10.07",
+        time: '12:23'
+      },
+      {
+        id: 2,
+        image: '/assets/image/ljj.jpg',
+        name: '张学友',
+        value: '大伊山、伊甸园、潮河湾、伊芦山……这个季节的灌云，也太美了叭！灌云县地处江苏省东北部、新沂河北岸',
+        calendar: "2020.10.07",
+        time: '12:23'
+      },
+      {
+        id: 3,
+        image: '/assets/image/ljj.jpg',
+        name: '周星驰',
+        value: '大伊山、伊甸园、潮河湾、伊芦山……这个季节的灌云，也太美了叭！灌云县地处江苏省东北部、新沂河北岸',
+        calendar: "2020.10.07",
+        time: '12:23'
+      },
+      {
+        id: 4,
+        image: '/assets/image/ljj.jpg',
+        name: '吴彦祖',
+        value: '大伊山、伊甸园、潮河湾、伊芦山……这个季节的灌云，也太美了叭！灌云县地处江苏省东北部、新沂河北岸',
+        calendar: "2020.10.07",
+        time: '12:23'
+      },
+      {
+        id: 5,
+        image: '/assets/image/ljj.jpg',
+        name: '刘德华',
+        value: '大伊山、伊甸园、潮河湾、伊芦山……这个季节的灌云，也太美了叭！灌云县地处江苏省东北部、新沂河北岸',
+        calendar: "2020.10.07",
+        time: '12:23'
+      },
+      {
+        id: 6,
+        image: '/assets/image/ljj.jpg',
+        name: '张国荣',
+        value: '大伊山、伊甸园、潮河湾、伊芦山……这个季节的灌云，也太美了叭！灌云县地处江苏省东北部、新沂河北岸',
+        calendar: "2020.10.07",
+        time: '12:23'
+      },
+    ]
+  }
+})
